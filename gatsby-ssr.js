93b96b3a3074d2577af0ba70695e292febd1e9d9
Kennedy Rose <kennedyianrose@gmail.com>
2019-04-29 10:00:15 -0500

    Removed clutter

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,15 +1,5 @@
-const React = require(`react`)
 console.log(`\n\n\nGATSBY SSR\n\n\n`)
 
 exports.onRenderBody = ({ setPostBodyComponents }) => {
 	console.log(`\n\n\nONRENDERBODY\n\n\n`)
-	process.exit(0)
-	setPostBodyComponents([
-		<link
-			key='leafletCss'
-			href='https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.4.0/leaflet.css'
-			rel='stylesheet'
-			type='text/css'
-		/>,
-	])
 }
